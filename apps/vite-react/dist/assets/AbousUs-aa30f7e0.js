370a0eedb5f685b3729ae7a73090bf5da5479c9e
Mai Vinh Thanh <maivinhthanh@Mai-Vinh-Thanh-Mac-Pro.local>
2023-09-05 22:14:40 +0700

    init again

--- apps/vite-react/dist/assets/AbousUs-aa30f7e0.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-38200837.js";const s=()=>t.jsx(t.Fragment,{children:t.jsx("div",{children:"AboutUs"})});export{s as default};
---
+++ packages/ui/dist/index.mjs
@@ -0,0 +1,1 @@
+export { a as Card } from './chunk-MFXFT5EY.mjs';
