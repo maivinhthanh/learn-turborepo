c29dc85461da5c8f2c7f26d7a53a22315b3eddb7
Mai Vinh Thanh <maivinhthanh@Mai-Vinh-Thanh-Mac-Pro.local>
2023-09-07 00:15:59 +0700

    add icon

---
+++ apps/web/src/app/character/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "ui";
+
+export default function Character(): JSX.Element {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Card title={""} href={""}>thanh</Card>
+    </main>
+  );
+}
